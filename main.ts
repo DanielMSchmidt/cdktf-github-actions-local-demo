2c9987699179a99b752ff307b796b5e49565c2e4
Daniel Schmidt <danielmschmidt92@gmail.com>
2022-06-09 10:45:07 +0200

    basic app

--- main.ts
+++ main.ts
@@ -1,11 +1,24 @@
 import { Construct } from "constructs";
 import { App, TerraformStack } from "cdktf";
+import * as n from "@cdktf/provider-null";
+import * as r from "@cdktf/provider-random";
 
 class MyStack extends TerraformStack {
   constructor(scope: Construct, name: string) {
     super(scope, name);
 
-    // define resources here
+    new n.NullProvider(this, "null");
+    new r.RandomProvider(this, "random");
+
+    const pet = new r.Pet(this, "my-pet");
+    const resource = new n.Resource(this, "hello-world");
+    resource.addOverride("provisioner", [
+      {
+        "local-exec": {
+          command: `echo 'Hello ${pet}'`,
+        },
+      },
+    ]);
   }
 }
 
